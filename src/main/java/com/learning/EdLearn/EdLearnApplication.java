2c76a59746cb7ff4b4554976c13136023d34dcac
hmmdnezami <hmmdnezami@gmail.com>
2024-01-15 20:24:42 +0530

    FirstCommit

---
+++ src/main/java/com/learning/EdLearn/EdLearnApplication.java
@@ -0,0 +1,13 @@
+package com.learning.EdLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/EdLearn/EdLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.EdLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
